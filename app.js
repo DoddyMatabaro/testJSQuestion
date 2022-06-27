92f9dce4ae54716f7c816e12140e6e11a17d4529
Doddy MATABARO <matabarododdy@gmail.com>
2022-06-27 15:02:11 +0200

    feat: added of takeReponsesList function

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ function Libelle(titre, reponse, reponses){ //class that structed questions
   this.reponses= reponses;
 }
 const Base = [ //questions base
-    new Libelle("Quel est le type d'un fichier javascript ?","C",[".j",".jsx", ".js",".ts"]),
-    new Libelle("La syntaxe correcte pour créer un objet vide en Js est :","A", ["let monObjet = {}", "let monObjet = []", "let monObjet = ()", "let monObjet = null"]),
-    new Libelle("Ces mots permettent d'initialiser une variable, sauf :","C", ["var", "const", "function", "let"], 2),
-    new Libelle("La bonne syntaxe pour écrire un commentaire sur ligne est :","A", ["// comment", "+ comment", "/* comment */", "# comment"], 0),
-    new Libelle("En le passant à la fonction isNaN, nous obtenons true :",'C', ["300", "-2", "'3'", "0"], 2),
-    new Libelle("Vanilla JavaScript est :", ["Un framework javascript","D", "Une librairie javascript", "Une marque de crême", "Du pur javascript"], 3),
-    new Libelle("Pour écrire sur la console Javacript on utilise :","A", ["console.log()", "console.write()", "console.print()", "alert()"], 0),
-    new Libelle("En JavaScript, les expressions regulières sont encadrées par :","C", ["'", "#", "/", "."], 2),
-    new Libelle("La balise qui permet d'inserer du code javascript est :","C", ["js", "script", "javascript", "link"], 1),
-    new Libelle("Pour executer JavaScript sur un serveur nous avons besion de :","C", ["ReactNative", "ReactJS", "NodeJs", "Composer"], 2),
-    new Libelle("Une variable non declarée aura pour valeur :","D", ["null", "0", "Object", "undefined"], 3),
-    new Libelle("Pour faire réference à un script js extene en html :","B", ["<script href='mon_script.js'>", "<script src='mon_script.js'>", "<link src='mon_script.js'>", "<rel link='mon_script.js'>"], 1),
-    new Libelle("Pour executer alert() chaque 5s, on écrira :","A", ["setInterval(alert, 5000)", "setTimeout(alert, 5)", "setInterval(alert, 5)", "forTime(alert, 5000)"], 0),
-    new Libelle("Le nom correct d'une variable est :","B", ["ma_variable", "ma variable", "ma-variable", "mavariable()"], 1),
-    new Libelle("L'object JavaScript qui gére le DOM est  :","D", ["HTMLElement", "DOM", "Node", "document"], 3)
+    new Libelle("Quel est le type d'un fichier javascript ?",2,[".j",".jsx", ".js",".ts"]),
+    new Libelle("La syntaxe correcte pour créer un objet vide en Js est :",0, ["let monObjet = {}", "let monObjet = []", "let monObjet = ()", "let monObjet = null"]),
+    new Libelle("Ces mots permettent d'initialiser une variable, sauf :",2, ["var", "const", "function", "let"]),
+    new Libelle("La bonne syntaxe pour écrire un commentaire sur ligne est :",0, ["// comment", "+ comment", "/* comment */", "# comment"]),
+    new Libelle("En le passant à la fonction isNaN, nous obtenons true :",2, ["300", "-2", "'3'", "0"]),
+    new Libelle("Vanilla JavaScript est :",3, ["Un framework javascript","Une librairie javascript", "Une marque de crême", "Du pur javascript"]),
+    new Libelle("Pour écrire sur la console Javacript on utilise :",0, ["console.log()", "console.write()", "console.print()", "alert()"]),
+    new Libelle("En JavaScript, les expressions regulières sont encadrées par :",2, ["'", "#", "/", "."]),
+    new Libelle("La balise qui permet d'inserer du code javascript est :",2, ["js", "script", "javascript", "link"]),
+    new Libelle("Pour executer JavaScript sur un serveur nous avons besion de :",2, ["ReactNative", "ReactJS", "NodeJs", "Composer"]),
+    new Libelle("Une variable non declarée aura pour valeur :",3, ["null", 0, "Object", "undefined"]),
+    new Libelle("Pour faire réference à un script js extene en html :",1, ["<script href='mon_script.js'>", "<script src='mon_script.js'>", "<link src='mon_script.js'>", "<rel link='mon_script.js'>"]),
+    new Libelle("Pour executer alert() chaque 5s, on écrira :",1, ["setInterval(alert, 5000)", "setTimeout(alert, 5)", "setInterval(alert, 5)", "forTime(alert, 5000)"]),
+    new Libelle("Le nom correct d'une variable est :",1, ["ma_variable", "ma variable", "ma-variable", "mavariable()"]),
+    new Libelle("L'object JavaScript qui gére le DOM est  :",3, ["HTMLElement", "DOM", "Node", "document"])
   ];
 const question =Object.assign(document.createElement("div"), {className: "question"});
 const columns = Object.assign(document.createElement("div"), {className: "columns"});
 const column = [document.createElement('div'),document.createElement('div'), document.createElement('div'),document.createElement('div')];
 const inputs = [document.createElement('input'),document.createElement('input'), document.createElement('input'),document.createElement('input')];
-const label = [document.createElement('label'),document.createElement('label'), document.createElement('label'),document.createElement('label')];
+const label = [document.createElement('label'),document.createElement('label'), document.createElement('label'),document.createElement('label')]; let reponses = new Array(); // 
 const quit = Object.assign(document.createElement("button"), {className: "quit", textContent: "Quitter"});
 const next = Object.assign(document.createElement("button"), {className: "next", textContent: "Suivant"});
 const btns = Object.assign(document.createElement("div"), {className: "column-btn"});
@@ -32,14 +32,14 @@ const head = Object.assign(document.createElement("div"), {className: "head2"});
 const progressCont = Object.assign(document.createElement("div"), {className: "progressCont"});
 const span1 = document.querySelector(".infoName"); const nom = document.querySelector("#nom");
 const span2 = document.querySelector(".infoMail"); const mail = document.querySelector("#mail");
-const form = document.querySelector("#form");let j = 0; //slide number;
+const form = document.querySelector("#form");let j = 0; //slide number 
 const progress = Object.assign(document.createElement("progress"), {value: "60", max: "60"});
 const progressTimer = Object.assign(document.createElement("div"), {className: "progressTimer", textContent: progress.value});
 progress.append(Object.assign(document.createElement("div"), {className: "barre"}));
 btns.append(quit, next);
 const start = document.querySelector(".start");
       for(let i=0; i<column.length; i++){
-        Object.assign(columns.appendChild(column[i]), {classList:"column-radio"}).append(Object.assign(column[i].appendChild(inputs[i]), {name: "reponse", type: "radio", id:""+i+"", value: Base[j].reponses[i]}),Object.assign(column[i].appendChild(label[i]), {for:""+i+"", textContent: Base[j].reponses[i]}));
+        Object.assign(columns.appendChild(column[i]), {classList:"column-radio"}).append(Object.assign(column[i].appendChild(inputs[i]), {name: "reponse", type: "radio", id:""+i+"", value: i}),Object.assign(column[i].appendChild(label[i]), {for:""+i+"", textContent: Base[j].reponses[i]}));
       }
       const progressText = Object.assign(document.createElement("div"), {className: "progressText", textContent: "Question "+ (j+1)+"/"+Base.length});
       progressCont.append(progressText, progressTimer, progress);
@@ -52,13 +52,21 @@ const start = document.querySelector(".start");
 
 let nextQuestion = ()=>{
   for(let i=0; i<column.length; i++){
-      inputs[i].value =  Base[j].reponses[i];
+      inputs[i].value =  i;
       label[i].textContent =  Base[j].reponses[i];
       progressText.textContent = "Question "+ (j+1)+"/"+Base.length;
       sujet.textContent =  Base[j].titre;
   }
 }
-for(const input of inputs){
+let checkSelectedAssertion = ()=>{
+    inputs.forEach((input)=>{
+        if(input.checked){
+            reponsesList(input.value);
+            return input.checked = false;
+        }
+    })
+}
+for(const input of inputs){//
   input.addEventListener('change', enabledNextButton);
 }        
 function enabledNextButton(e) {
@@ -66,12 +74,14 @@ function enabledNextButton(e) {
     next.disabled = false
   }
 }
+let reponsesList = (reponse)=>{
+    return reponses.push(reponse);
+}
 const move = () => {
 	if (progress.value > 0) {
-    progressTimer.textContent = progress.value;
-		 progress.value--;
-     
-		setTimeout(move, 1000);
+     progressTimer.textContent = progress.value;
+		 progress.value--;     
+		 setTimeout(move, 1000);
 	} else {
 		// checkResponse()
 		j++
@@ -82,8 +92,10 @@ const move = () => {
 };
 next.addEventListener("click", (e)=>{ // next question event
   e.preventDefault();
-   
+  
   if(j < Base.length){
+    checkSelectedAssertion();
+    console.log();
     j++;
     nextQuestion();
     progress.value = 60;
@@ -101,4 +113,3 @@ start.addEventListener("click",(e)=>{ //start button event
         e.preventDefault();
         test([nom,mail], [span1,span2])
 })
-
