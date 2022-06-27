52ecc0fa0f53b85df279335bddd809a5ec1dd65e
Doddy MATABARO <matabarododdy@gmail.com>
2022-06-27 10:50:35 +0200

    refact: reajust of move function

--- app.js
+++ app.js
@@ -29,23 +29,21 @@ const quit = Object.assign(document.createElement("button"), {className: "quit",
 const next = Object.assign(document.createElement("button"), {className: "next", textContent: "Suivant"});
 const btns = Object.assign(document.createElement("div"), {className: "column-btn"});
 const head = Object.assign(document.createElement("div"), {className: "head2"});
+const progressCont = Object.assign(document.createElement("div"), {className: "progressCont"});
 const span1 = document.querySelector(".infoName"); const nom = document.querySelector("#nom");
 const span2 = document.querySelector(".infoMail"); const mail = document.querySelector("#mail");
-const form = document.querySelector("#form");
-const start = document.querySelector(".start");
 
-const progressCont = Object.assign(document.createElement("div"), {className: "progressCont"});
-const progress = Object.assign(document.createElement("div"), {className: "progress"});
-const progressTimer = Object.assign(document.createElement("div"), {className: "progressTimer", textContent: "30"});
+const form = document.querySelector("#form");let j = 0; //slide number;
+const progress = Object.assign(document.createElement("progress"), {value: "60", max: "60"});
+const progressTimer = Object.assign(document.createElement("div"), {className: "progressTimer", textContent: progress.value});
 progress.append(Object.assign(document.createElement("div"), {className: "barre"}));
 btns.append(quit, next);
-let j = 0; //slide number;
-
+const start = document.querySelector(".start");
 function affichePage(Base, question, j, column, columns, input,label,progressCont){
       for(let i=0; i<column.length; i++){
         Object.assign(columns.appendChild(column[i]), {classList:"column-radio"}).append(Object.assign(column[i].appendChild(input[i]), {name: "reponse", type: "radio", id:""+i+"", value: Base[j].reponses[i]}),Object.assign(column[i].appendChild(label[i]), {for:""+i+"", textContent: Base[j].reponses[i]}));
       }
-      const progressText = Object.assign(document.createElement("div"), {className: "progressText", textContent: j+1+"/"+Base.length});
+      const progressText = Object.assign(document.createElement("div"), {className: "progressText", textContent: "Question "+ (j+1)+"/"+Base.length});
       progressCont.append(progressText, progressTimer, progress);
       head.appendChild(Object.assign(document.createElement("h1"), { textContent: Base[j].titre}));
       head.appendChild(progressCont);
@@ -53,51 +51,37 @@ function affichePage(Base, question, j, column, columns, input,label,progressCon
     question.append(head, columns);
     return question;
 }
-console.log(question);
+const move = () => {
+	if (progress.value > 0) {
+    progressTimer.textContent = progress.value;
+		 progress.value--;
+     
+		setTimeout(move, 1000);
+	} else {
+		// checkResponse()
+		progress.value = 60;
+		j++
+	}
+};
 next.addEventListener("click", (e)=>{
+  e.preventDefault();
   if(j < Base.length){
     j++;
-    e.preventDefault();
-    form.appendChild(affichePage(Base,question,j,column,columns,input,label,  progressCont));
-    move(0,false)
-    move(100, true);
+    form.appendChild(affichePage(Base,question,j,column,columns,input,label, progressCont));
+    // move(100, true);
   }
 })
 
 let validateEmail =  (emailAdress)=>{ //email validation
-      let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      return (emailAdress.match(regexEmail) ? true : false);
+      let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+      return regexEmail.test(emailAdress) ;
  }
-
-let test = (elt, info, text)=>{ // form validation
-  return (text == "nom" ? (elt.value == "" ?  info.textContent = "Veillez renseigner votre nom" : (elt.value.length <= 2 ?  info.textContent = "Entrer un nom valide" : info.textContent = "") ) : (elt.value == "" ?  info.textContent = "Veillez renseigner votre mail" : (validateEmail(elt.value) ?  info.textContent = "Entrer une adresse mail valide" : 
-        document.querySelector(".accueil").style.display = "none"
-          
-   ) ));
+let test = (elt, info)=>{ // form validation
+     return (elt[0].value == "" ? info[0].textContent = "Veillez renseigner votre nom" : elt[0].value.length <= 2 ? info[0].textContent ="Entrer un nom valide": elt[1].value == "" ? info[1].textContent = "Veuillez renseigner votre mail" : !validateEmail(elt[1].value) ? info[1].textContent = "Entrer une adresse mail valide" :
+      (document.querySelector(".accueil").style.display = "none",form.appendChild(affichePage(Base,question,j,column,columns,input,label,progressCont)), move()));
 }
 start.addEventListener("click",(e)=>{ //start button event 
         e.preventDefault();
-        test(nom, span1, "nom");
-        test(mail, span2, "mail");
-        
-        move(100, true);
-
+        test([nom,mail], [span1,span2])
 })
-    function move(i) {
-      if (i == 100) {
-        i = 99;
-        let elem = document.querySelector(".barre");
-        let width = 99;
-        let id = setInterval(frame, 600);
-        function frame() {
-          if (width <= 0) {
-            clearInterval(id);
-            i = 100;
-          } else {
-            width--;
-            elem.style.width = width + "%";
-          }
-        }
-      }
-    }
 
